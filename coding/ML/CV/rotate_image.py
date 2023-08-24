fd212f60b02a0c0a7027c93f8ff7fdad4d6d19b9
PradeepKadubandi <pradeep.v.kadubandi@gmail.com>
2023-08-23 18:40:58 -0700

    rotate image (by specific and arbitrary angle).
    And additional links to interview questions.

---
+++ coding/ML/CV/rotate_image.py
@@ -0,0 +1,9 @@
+# Problem: 
+#There is an image represented as an m * n matrix. 
+#1) Rotate the image by 90, 180, 270, 360
+#2) Rotate the image by a custom angle.
+#Return the rotated image.
+
+
+# The below article seems to be the complete answer!
+# https://www.educative.io/answers/opencv-rotate-image 
